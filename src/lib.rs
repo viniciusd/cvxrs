4bba178956ae1026e3e84bedf6171ecb26f52580
Vinícius Dantas <viniciusdantas@bct.ect.ufrn.br>
2019-11-10 17:01:33 -0300

    src/functions.rs
    Update building blocks for convex problems

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,41 @@
+// Gonna be useful:
+// https://crates.io/crates/ndarray-rand
+// https://github.com/rust-ndarray/ndarray-linalg
+
+mod functions;
 mod variable;
-
-pub use crate::variable::Variable;
-
 
 #[cfg(test)]
 mod tests {
-use crate::variable::Variable;
+    use crate::functions::Function;
+    use crate::variable::*;
+    use ndarray::prelude::*;
     #[test]
     fn it_works() {
-        let v = Variable::new(&[2]);
-        assert_eq!(v.shape[0], 3);
+        let x = Variable::new(&[3]);
+        let a = array![
+            [1.62, -0.61, -0.52],
+            [-1.07, 0.86, -2.30],
+            [1.74, -0.76, 0.31],
+            [-0.24, 1.46, -2.06],
+        ];
+        let a = Constant::new(&[], a.view());
+        let b = array![[-0.32, -0.38, 1.13, -1.09]];
+        let b = Constant::new(&[], b.view());
+
+        let objective = Expression::Function(
+            Function::Subtract(
+                Expression::Function(Function::Multiply(a.into(), x.clone().into()).into()),
+                b.into(),
+            )
+            .into(),
+        );
+
+        let problem = Problem {
+            objective,
+            equalities: &Vec::new(),
+            inequalities: &Vec::new(),
+        };
+        assert_eq!(x.shape[0], 3);
     }
 }
--- src/variable.rs
+++ src/variable.rs
@@ -1,51 +1,75 @@
+use crate::functions;
+use ndarray::ArrayView;
+
+pub struct Problem<'a, A, D> {
+    pub objective: Expression<'a, A, D>,
+    pub equalities: &'a [Relation<'a, A, D>],
+    pub inequalities: &'a [Relation<'a, A, D>],
+}
+
+pub struct Relation<'a, A, D> {
+    pub rhs: Expression<'a, A, D>,
+    pub lhs: Expression<'a, A, D>,
+}
+
+#[derive(Clone)]
 pub struct Variable<'a> {
-    pub shape: &'a[u32]
+    pub shape: &'a [u32],
 }
 
 impl<'a> Variable<'a> {
-	pub fn new(shape: &'a[u32]) -> Variable {
-		Variable {
-            shape
-        }
-	}
+    pub fn new(shape: &'a [u32]) -> Variable {
+        Variable { shape }
+    }
 }
 
-pub struct Constant<'a> {
-    pub shape: &'a[u32]
+pub struct Constant<'a, A, D> {
+    pub shape: &'a [u32],
+    pub value: ArrayView<'a, A, D>,
 }
 
-impl<'a> Constant<'a> {
-	pub fn new(shape: &'a[u32]) -> Constant {
-		Constant {
-            shape
-        }
-	}
-}
-
-pub trait Function {
-	fn sign(&self) -> Option<Sign>;
-	fn curvature(&self) -> Option<Curvature>;
-	fn monotonicity(&self) -> Option<Monotonicity>;
+impl<'a, A, D> Constant<'a, A, D> {
+    pub fn new(shape: &'a [u32], value: ArrayView<'a, A, D>) -> Constant<'a, A, D> {
+        Constant { shape, value }
+    }
 }
 
 pub enum Sign {
-	Negative,
-	Positive,
+    Negative,
+    Positive,
 }
 
 pub enum Curvature {
-	Convex,
-	Concave,
-	Affine,
+    Convex,
+    Concave,
+    Affine,
 }
 
 pub enum Monotonicity {
-	Nondecreasing,
-	Nonincreasing,
+    Nondecreasing,
+    Nonincreasing,
 }
 
-pub enum Expression<'a> {
+pub enum Expression<'a, A, D> {
     Variable(Variable<'a>),
-    Constant(Constant<'a>),
-    Function(Box<dyn Function>),
+    Constant(Constant<'a, A, D>),
+    Function(Box<functions::Function<'a, A, D>>),
+}
+
+impl<'a, A, D> From<Variable<'a>> for Expression<'a, A, D> {
+    fn from(item: Variable<'a>) -> Self {
+        Expression::Variable(item)
+    }
+}
+
+impl<'a, A, D> From<Constant<'a, A, D>> for Expression<'a, A, D> {
+    fn from(item: Constant<'a, A, D>) -> Self {
+        Expression::Constant(item)
+    }
+}
+
+impl<'a, A, D> From<functions::Function<'a, A, D>> for Expression<'a, A, D> {
+    fn from(item: functions::Function<'a, A, D>) -> Self {
+        Expression::Function(Box::new(item))
+    }
 }
