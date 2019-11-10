e33265599b52f45c7d2256bc35e29ad6805e2186
Vinícius Dantas <viniciusdantas@bct.ect.ufrn.br>
2019-11-10 18:38:04 -0300

    Refactor functions.rs

--- src/functions.rs
+++ src/functions.rs
@@ -1,5 +1,33 @@
-use crate::variable::Expression;
+use crate::variable::{Curvature, Expression, Monotonicity, Sign};
 
-enum functions {
-	multiply(u32, Expression),
+pub enum Function<'a, A, D> {
+    Multiply(Expression<'a, A, D>, Expression<'a, A, D>),
+    Subtract(Expression<'a, A, D>, Expression<'a, A, D>),
+}
+
+trait Atom {
+    fn sign(&self) -> Option<Sign>;
+    fn curvature(&self) -> Option<Curvature>;
+    fn monotonicity(&self) -> Option<Monotonicity>;
+}
+
+impl<'a, A, D> Atom for Function<'a, A, D> {
+    fn sign(&self) -> Option<Sign> {
+        match self {
+            Function::Multiply(_, _) => None,
+            Function::Subtract(_, _) => None,
+        }
+    }
+    fn curvature(&self) -> Option<Curvature> {
+        match self {
+            Function::Multiply(_, _) => Some(Curvature::Convex),
+            Function::Subtract(_, _) => Some(Curvature::Affine),
+        }
+    }
+    fn monotonicity(&self) -> Option<Monotonicity> {
+        match self {
+            Function::Multiply(_, _) => Some(Monotonicity::Nonincreasing),
+            Function::Subtract(_, _) => Some(Monotonicity::Nonincreasing),
+        }
+    }
 }
